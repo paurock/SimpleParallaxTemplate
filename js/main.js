b8c082f4c9f6ea28e0bdde80ea1857d24f861daf
Roman T <tselenkor@gmail.com>
2019-09-07 11:47:13 +0300

    added responsiveness

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.navbar a').on('click', function (e) {
+/* $('.navbar a').on('click', function (e) {
     if (this.hash !== '') {
         e.preventDefault();
 
@@ -9,4 +9,35 @@ $('.navbar a').on('click', function (e) {
                 scrollTop: $(hash).offset().top
             }, 800);
     }
-});
+}); */
+//MENU HAMBURGER//
+// Select DOM Items
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
+    }
+}
